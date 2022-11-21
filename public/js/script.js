87310552f02eb00d9d1fbc080ca7199233bd69dd
watataku8911 <watato.takuya@gmail.com>
2022-11-21 18:38:20 +0900

    Tech Blogページ完成

--- public/js/script.js
+++ public/js/script.js
@@ -30,3 +30,21 @@ const intersection = (domElements) => {
     observer.observe(domElement);
   });
 };
+
+const smoothScroll = () => {
+  const scrollElement = document.querySelectorAll("a[href^='#']");
+  for (let i = 0; i < scrollElement.length; i++) {
+    scrollElement[i].addEventListener("click", (e) => {
+      e.preventDefault();
+      const href = scrollElement[i].getAttribute("href");
+      const target = document.getElementById(href.replace("#", ""));
+      const rect = target.getBoundingClientRect().top;
+      const offset = window.pageYOffset;
+      const position = rect + offset;
+      window.scrollTo({
+        top: position,
+        behavior: "smooth",
+      });
+    });
+  }
+};
