4f8d4d8b47bbe30f721ddfb5d01ba66bf6422b0c
watataku8911 <watato.takuya@gmail.com>
2023-07-28 12:28:53 +0900

    twitterのsvg変更

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import styles from "./Footer.module.css";
 
-import { ReactSVG } from "react-svg";
-
-import IconTw from "/icon/icon_twitter_black.svg";
-import IconInst from "/icon/ico_inst_foot.svg";
-import IconGit from "/icon/github_logo_small.svg";
-
 const Footer = () => {
   return (
     <footer className={styles["footer"]}>
@@ -46,34 +40,6 @@ const Footer = () => {
 
       <div className={`${styles["footer__copy"]} ${styles["grid"]}`}>
         <small>&copy;2022 Watataku.</small>
-        <div className={`${styles["footer__copy__icons"]} ${styles["grid"]}`}>
-          <a
-            href="https://twitter.com/watataku8911"
-            aria-label="Twitter"
-            target="_blank"
-          >
-            <ReactSVG style={{ width: "25px", height: "25px" }} src={IconTw} />
-          </a>
-
-          <a
-            href="https://instagram.com/watataku8911"
-            aria-label="Instagram"
-            target="_blank"
-          >
-            <ReactSVG
-              style={{ width: "25px", height: "25px" }}
-              src={IconInst}
-            />
-          </a>
-
-          <a
-            href="https://github.com/watataku8911"
-            aria-label="Github"
-            target="_blank"
-          >
-            <ReactSVG style={{ width: "25px", height: "25px" }} src={IconGit} />
-          </a>
-        </div>
       </div>
     </footer>
   );
