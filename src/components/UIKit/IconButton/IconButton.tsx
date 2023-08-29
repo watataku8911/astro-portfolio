4b7299441b61f26561f938ddf1c72e7b40aed15a
watataku8911 <watato.takuya@gmail.com>
2023-08-29 23:16:52 +0900

    リンク切れを治した

--- src/components/UIKit/IconButton/IconButton.tsx
+++ src/components/UIKit/IconButton/IconButton.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-
 import styles from "./IconButton.module.css";
 
 type Props = {
   href: string;
-  target: "_blank" | "_self" | "_top" | "_parent";
   icon: string;
   alt: string;
 };
 const IconButton = (props: Props) => {
   return (
-    <a className={styles.link} href={props.href} target={props.target}>
+    <a
+      className={styles.link}
+      href={props.href}
+      target="_blank"
+      rel="nofollow noopener noreferrer"
+    >
       <p className={styles["icon-button"]}>
         <img className={styles.icon} src={props.icon} alt={props.alt} />
       </p>
